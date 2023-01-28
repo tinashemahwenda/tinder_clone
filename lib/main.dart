98224fb26a6021dcbb9726cc41bedf9b7c5e980a
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2023-01-28 18:03:52 +0200

    Created a fully functional bottom navigation

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
+    return MaterialApp(
+      home: const HomePage(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.amber, fontFamily: 'Gotham'),
     );
   }
 }
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,10 +1,59 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int selectedIndex = 0;
+
+  @override
+  void BottomNavigator(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  final List<Widget> _children = [
+    const Center(
+      child: Text('Home'),
+    ),
+    const Center(
+      child: Text('Explore'),
+    ),
+    const Center(
+      child: Text('Likes'),
+    ),
+    const Center(
+      child: Text('Chat'),
+    ),
+    const Center(
+      child: Text('Account'),
+    ),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: _children[selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        type: BottomNavigationBarType.fixed,
+        onTap: BottomNavigator,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.heat_pump),
+            label: '',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.chat), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.people), label: ''),
+        ],
+      ),
+    );
   }
 }
