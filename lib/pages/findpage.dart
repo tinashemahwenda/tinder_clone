8357e4cc0abf7135eddf230acd1a27f939a8fbf5
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2023-01-28 18:08:03 +0200

    Created an appBar

---
+++ lib/pages/findpage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class findPage extends StatelessWidget {
+  const findPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Row(
+                children: const [],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
