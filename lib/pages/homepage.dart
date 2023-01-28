6a9d20e87cc39685cebf749964501bf3c7fa31a5
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2023-01-28 18:10:08 +0200

    Created a findPage view in homepage

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tinder_clone/pages/findpage.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,9 +19,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   final List<Widget> _children = [
-    const Center(
-      child: Text('Home'),
-    ),
+    const findPage(),
     const Center(
       child: Text('Explore'),
     ),
